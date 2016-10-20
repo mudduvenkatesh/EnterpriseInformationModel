9ddad7fd264b9f64fe0ffeb4e4627c3489e71f92
Venkatesh Muddu <venkatesh.muddu@bnymellon.com>
2016-10-20 06:30:48 -0400

    updated EIM RDF and EIM Vocabulary

--- src/main/java/net/trigyan/glide/spec/vocabulary/EIM.java
+++ src/main/java/net/trigyan/glide/spec/vocabulary/EIM.java
@@ -44,18 +44,26 @@ public class EIM {
 	public static final IRI SOFTWARE_ASSET;
 	public static final IRI TRANSACTION_DATA;
 	public static final IRI VALIDITY;
+	public static final IRI DATA_OWNER;
+	public static final IRI DATA_STEWARD;
+	public static final IRI DATA_CUSTODIAN;
 	public static final IRI DATA_CLASSIFIED_BY;
-	public static final IRI HAS_DATA_CUSTODIAN;
-	public static final IRI HAS_DATA_OWNER;
-	public static final IRI HAS_DATA_STEWARD;
 	public static final IRI IS_AUTHORATATIVE_SOURCE_FOR;
 	public static final IRI IS_DATA_PROVIDER_FOR;
-	public static final IRI IS_GOVERNED_BY;
 	public static final IRI HAS_CODE;
 	public static final IRI HAS_DESCRIPTION;
 	public static final IRI HAS_DISPLAY_NAME;
 	public static final IRI HAS_ID;
 	public static final IRI HAS_NAME;
+	public static final IRI IS_GOVERNED_BY;
+	public static final IRI IS_MANAGED_BY;
+	public static final IRI DATA_VISIBIITY_CLASSIFICATION;
+	public static  final IRI PUBLIC;
+	public static final IRI INTERNAL;
+	public static final IRI CONFIDENTIAL;
+	public static final IRI HCI;
+	public static final IRI PID;
+	public static final IRI CID;
 	 
 	static {
 		ValueFactory factory = SimpleValueFactory.getInstance();
@@ -85,18 +93,29 @@ public class EIM {
 		EVENT_DATA=factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#EventData");
 		FINANCIAL_EVENT=factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#FinancialEvent");
 		VALIDITY=factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#Validity");
+		DATA_OWNER = factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#DataOwner");
+		DATA_CUSTODIAN=factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#DataCustodian");
+		DATA_STEWARD=factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#DataSteward");
 		DATA_CLASSIFIED_BY = factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#data_classified_by");
-		HAS_DATA_CUSTODIAN = factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#has_data_custodian");
-		HAS_DATA_OWNER = factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#has_data_owner");
-		HAS_DATA_STEWARD = factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#has_data_steward");
+		//HAS_DATA_CUSTODIAN = factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#has_data_custodian");
+		//HAS_DATA_OWNER = factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#has_data_owner");
+		//HAS_DATA_STEWARD = factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#has_data_steward");
 		IS_AUTHORATATIVE_SOURCE_FOR = factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#is_authoratative_source_for");
 		IS_DATA_PROVIDER_FOR = factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#is_data_provider_for");
 		IS_GOVERNED_BY = factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#is_governed_by");
+		IS_MANAGED_BY = factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#is_managed_by");
 		HAS_CODE = factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#hasCode");
 		HAS_DESCRIPTION = factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#hasDescription");
 		HAS_DISPLAY_NAME = factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#hasDisplayName");
 		HAS_ID = factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#hasId");
 		HAS_NAME = factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#hasName");
+		DATA_VISIBIITY_CLASSIFICATION = factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#DataVisibiityClassification");
+		PUBLIC = factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#Public");
+		INTERNAL=factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#Internal");
+		CONFIDENTIAL=factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#Confidential");
+		HCI = factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#HCI");
+		PID = factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#PID");
+		CID = factory.createIRI("http://spec.trigyan.net/glide/EnterpriseInformationModel#CID");
 		 
 	}
 	
